9f72c73d997389365c31b78de1aa501156068bf0
lcruz <lcruz@intelligensense.com>
2016-01-13 14:39:35 -0600

    Changes for future techtakls

--- app.js
+++ app.js
@@ -1,3 +1,18 @@
+/**
+ * Copyright 2009 Intelligent Sense.
+ * All Rights Reserved.
+ * 
+ * This software is the propietary information of 
+ * Intelligent Sense.
+ * Use this subject to license terms.
+ *
+ * Filename: app.js
+ */
+
+/**
+ * Module to test if the package is working
+ * @return {void} 
+ */
 exports.testModule = function() {
-  console.log("Node module is working correctly");
+  console.log("Node module from git is working correctly");
 }
